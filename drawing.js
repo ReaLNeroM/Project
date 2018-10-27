051769588826933648b782ba54d0060d9e5929ed
ReaLNeroM <vlade.maksimovski@gmail.com>
2018-10-27 14:28:50 -0400

    Lowered number of points

--- drawing.js
+++ drawing.js
@@ -8,13 +8,13 @@ var size = Math.min(canvas.width, canvas.height) / 2;
 
 var pointsX = [];
 var pointsY = [];
-pointsX.length = 1000;
-pointsY.length = 1000;
+pointsX.length = 300;
+pointsY.length = 300;
 
 var repulsorX = [800, 100];
 var repulsorY = [800, 100];
 
-for (var i = 0; i < 1000; i++){
+for (var i = 0; i < 300; i++){
 	pointsX[i] = Math.floor(Math.random() * canvas.height);
 	pointsY[i] = Math.floor(Math.random() * canvas.width);
 }
@@ -23,16 +23,18 @@ main();
 function main(){
 	ctx.fillStyle = "#fff";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "#ff0";
+	ctx.fillRect(0, 0, 50, 50);
 
 	ctx.fillStyle = "#f00";
 	ctx.fillRect(repulsorX[0], repulsorY[0], 4, 4);
 	ctx.fillRect(repulsorX[1], repulsorY[1], 4, 4);
 	var forceX = [];
 	var forceY = [];
-	forceX.length = 1000;
-	forceY.length = 1000;
+	forceX.length = 300;
+	forceY.length = 300;
 
-	for(var i = 0; i < 1000; i++){
+	for(var i = 0; i < 300; i++){
 		forceX[i] = 0;
 		forceY[i] = 0;
 	}
@@ -54,7 +56,7 @@ function main(){
 		pointsX[i] += forceX[i];
 	}
 
-	for (var i = 0; i < 1000; i++){
+	for (var i = 0; i < 300; i++){
 		ctx.fillStyle = "#00f";
 		ctx.fillRect(pointsX[i], pointsY[i], 4, 4);
 	}
